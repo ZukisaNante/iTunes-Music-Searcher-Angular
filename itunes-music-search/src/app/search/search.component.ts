1135bf68f5abebff477e2ffd0887b00f22e06244
ZukisaNante <zukisanante@gmail.com>
2020-02-28 17:42:15 +0100

    search form created and working

--- itunes-music-search/src/app/search/search.component.ts
+++ itunes-music-search/src/app/search/search.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators }from "@angular/forms";
 import { Subject, throwError } from 'rxjs';
 
-import { map, debounceTime, distinctUntilChanged, switchMap, catchError, retry } from 'rxjs/operators';
+import { map, debounceTime, distinctUntilChanged, switchMap, catchError } from 'rxjs/operators';
+import { SearchService } from '../search.service';
+
 
 @Component({
   selector: 'app-search',
@@ -11,9 +13,45 @@ import { map, debounceTime, distinctUntilChanged, switchMap, catchError, retry }
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private searchService: SearchService) { }
 
-  ngOnInit(): void {
+  public loading: boolean;
+  public searchTerm = new Subject<string>();
+  public searchResults: any;
+  public paginationElements: any;
+  public errorMessage: any;
+
+  public searchForm = new FormGroup({
+    search: new FormControl('', Validators.required),
+  });
+
+  public search(){
+    this.searchTerm.pipe(
+      map((e: any) => {
+        console.log(e.target.value);
+        return e.target.value
+      }),
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap(term => {
+        this.loading = true;
+        return this.searchService._searchEntries(term);
+      }),
+      catchError((e) =>{
+        console.log(e);
+        this.loading = false;
+        this.errorMessage = e.message;
+        return throwError(e);
+      }),
+    ).subscribe(v => {
+      this.loading = true;
+      this.searchResults = v;
+      this.paginationElements = this.searchResults;
+    })
+  }
+
+  ngOnInit() {
+    this.search();
   }
 
 }
